8bebe34051de3f62721e074ad47a6c02621e95dd
Ashwanth2310 <ashwanth@rajakumar.net>
2024-07-21 11:28:32 +0530

    Start Dev on Nothing Tracker

--- App.tsx
+++ App.tsx
@@ -1,20 +1,68 @@
+import * as React from "react"
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
+import * as FileSystem from "expo-file-system"
+import {Asset} from "expo-asset"
+import {SQLiteProvider} from "expo-sqlite/next"
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+import Home from "./screens/Home";
 
+const Stack = createNativeStackNavigator();
+
+const loadDataBase = async() =>{
+  const dbName = "nothingDB.db"
+  const dbAsset = require("./assets/nothingDB.db")
+  const dbUri = Asset.fromModule(dbAsset).uri;
+  const dbFilePath = `${FileSystem.documentDirectory}SQLite/${dbName}`
+
+  const fileInfo = await FileSystem.getInfoAsync(dbFilePath);
+  if(!fileInfo.exists){
+    await FileSystem.makeDirectoryAsync(
+      `${FileSystem.documentDirectory}SQLite`,
+      {intermediates:true}
+    );
+    await FileSystem.downloadAsync(dbUri,dbFilePath)
+  }
+}
 export default function App() {
+  const [dbLoaded,setDbLoaded] = React.useState<boolean>(false)
+  React.useEffect(()=>{
+    loadDataBase()
+    .then(()=> setDbLoaded(true))
+    .catch((e) => console.error(e))
+  })
+  if(!dbLoaded) return(
+  <View style={{flex:1}}>
+    <ActivityIndicator size = {"large"}/>
+    <Text>Loading...</Text>
+  </View>
+  )
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+    <React.Suspense
+    fallback = {
+      <View style={{flex:1}}>
+        <ActivityIndicator size = {"large"}/>
+        <Text>Loading...</Text>
+      </View>
+      }
+    >
+      <SQLiteProvider databaseName="nothingDB.db" useSuspense>
+        <Stack.Navigator>
+          <Stack.Screen
+          name = "Home"
+          component={Home}
+          options={{
+            headerLargeTitle: true,
+            headerTitle:"💲Nothing Budget ⬛"
+          }}
+          />
+        </Stack.Navigator>
+      </SQLiteProvider>
+    </React.Suspense>
+    </NavigationContainer>
+    
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
